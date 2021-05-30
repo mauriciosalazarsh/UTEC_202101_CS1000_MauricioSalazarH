8fe650d8312ee0702d2d7037ec9b754287fdcf99
Mauricio Salazar Hillenbrand <mauricio.salazar@utec.edu.pe>
2021-05-29 23:19:08 -0500

    Sem2Replit6

---
+++ Sem2Replit6.py
@@ -0,0 +1,39 @@
+num = 104
+
+base = 2
+# Primera división
+residuo = num % base
+cociente = num // base
+bit1 = residuo
+
+# Segunda división
+residuo = cociente % base
+cociente = cociente // base
+bit2 = residuo
+
+# Tercera división
+residuo = cociente % base
+cociente = cociente // base
+bit3 = residuo
+
+# Cuarta división
+residuo = cociente % base
+cociente = cociente // base
+bit4 = residuo
+
+# Quinta división
+residuo = cociente % base
+cociente = cociente // base
+bit5 = residuo
+
+# Sexta división
+residuo = cociente % base
+cociente = cociente // base
+bit6 = residuo
+
+# Séptima división
+residuo = cociente % base
+cociente = cociente // base
+bit7 = residuo
+
+print("{} = {}{}{}{}{}{}{}".format(num,bit7,bit6,bit5,bit4,bit3,bit2,bit1))
