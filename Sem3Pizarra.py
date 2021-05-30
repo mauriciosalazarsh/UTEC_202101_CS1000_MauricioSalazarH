529850e369eb372bfebdf627bad2da8de215981e
Mauricio Salazar Hillenbrand <mauricio.salazar@utec.edu.pe>
2021-05-29 23:39:02 -0500

    Sem3Pizarra

---
+++ Sem3Pizarra.py
@@ -0,0 +1,18 @@
+num6 = "10"
+num1 = int(num6)
+print(num1)
+
+num7 = '0b110'
+num2 = int(num7,2)
+print(num2)
+
+num8 = '0o10'
+decimal = int(num8,8)
+print(decimal)
+
+num9 = 'ox12'
+decimal1 = int(num9,16)
+print(decimal1)
+
+
+
